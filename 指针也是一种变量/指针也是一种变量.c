d0e59f180f1d32e01e09d8db4a9504ab8b3ce5ea
congzengguang <congzengguang@sina.cn>
2018-12-21 21:33:00 +0800

    指针也是一种变量类型

---
+++ 指针也是一种变量/指针也是一种变量.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int *p = NULL;
+	char *************pp = NULL;
+
+	printf("sizeof(p) = %d, sizeof(pp) = %d", sizeof(p), sizeof(pp));
+
+	getchar();
+	return 0;
+}
