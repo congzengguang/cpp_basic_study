7137de3b2d2624516fcc8eb2fa1532b0eed24121
cpp <cpp@cpp.com>
2020-02-15 12:03:02 +0800

    设计模式

---
+++ 设计模式_cpp/BankWorker.cpp
@@ -0,0 +1,15 @@
+#include "BankWorker.h"
+using namespace std;
+
+
+void BankWorker::save(){
+	cout << "´æ¿î" << endl;
+}
+void BankWorker::transfer() {
+	cout << "×ªÕË" << endl;
+}
+void BankWorker::pay() {
+	cout << "½É·Ñ" << endl;
+}
+
+
---
+++ 设计模式_cpp/BankWorker.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+class BankWorker {
+public:
+	void save();
+	void transfer();
+	void pay();
+};
+
